e383df7bc6798fd93e0287e50da054651df5e768
Will <WillKirkmanM@gmail.com>
2023-02-08 22:32:47 +0000

    Initial Files

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::io::stdin;
+
+fn main() {
+    println!("Input the Message You Would Like to Encrypt:");
+    
+    let mut message = String::new();
+
+    stdin()
+        .read_line(&mut message)
+        .expect("Could Not Read Input");
+
+    let alphabet = vec!["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    message.truncate(message.len() - 2);
+    let chars = message.chars();
+    for char in chars {
+        println!("{:?}", char)
+    }
+}
