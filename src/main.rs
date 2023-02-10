adfe52729ea959a38ed7f0797d2d00d67a4d066c
Will <WillKirkmanM@gmail.com>
2023-02-10 22:55:20 +0000

    Feat: Ciphers Message 🎉

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,25 @@ fn main() {
         .read_line(&mut message)
         .expect("Could Not Read Input");
 
-    let alphabet = vec!["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    let alphabet = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
+    let mut result: Vec<char> = vec![];
 
     message.truncate(message.len() - 2);
     let chars = message.chars();
-    for char in chars {
-        println!("{:?}", char)
+    for (count, mut char) in chars.enumerate() {
+        let mut index = alphabet.iter().position(|&v| v == char).unwrap_or(25 /* Space Character in Vector */);
+        println!("IDX: {:?} WRD: {:?}", index, char);
+
+        match index {
+            25 => { index = index - 1 }
+            26 => { index = 25 }
+            _ => {}
+        }
+
+        char = alphabet[index + 1];
+
+        result.insert(count, char)
     }
+    let message: String = result.iter().collect();
+    println!("Ciphered Message: {:?}", message)
 }
