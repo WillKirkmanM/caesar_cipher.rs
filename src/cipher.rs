07c5a5a2fc8c326fb36117eac209a97d90cb12d2
Will <WillKirkmanM@gmail.com>
2023-02-11 15:48:06 +0000

    Feat: Space Compatibility

---
+++ src/cipher.rs
@@ -0,0 +1,34 @@
+use std::{io::{stdin, stdout, Write}, ops::Neg};
+
+pub fn cipher() {
+  print!("Input the Message You Would Like to Encrypt: ");
+    let _ = stdout().flush();
+
+    let mut message = String::new();
+
+    stdin()
+        .read_line(&mut message)
+        .expect("Could Not Read Input");
+
+    let alphabet = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
+    let mut result: Vec<char> = vec![];
+
+    message.truncate(message.len() - 2);
+    let chars = message.chars();
+    for (count, mut char) in chars.enumerate() {
+        let mut index = alphabet.iter().position(|&v| v == char).unwrap_or(26 /* Space Character in Vector */);
+
+        match index {
+          25 => index = 0,
+          26 => index = 26,
+          _ => index = index + 1
+        }
+
+        char = alphabet[index];
+
+        result.insert(count, char)
+    }
+    let message: String = result.iter().collect();
+    println!("Ciphered Message: {:?}", message)
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,10 @@
-use std::io::{stdin, stdout, Write};
+mod cipher;
+mod init;
+
+use init::init;
+use cipher::cipher;
 
 fn main() {
-    print!("Input the Message You Would Like to Encrypt: ");
-    let _ = stdout().flush();
-
-    let mut message = String::new();
-
-    stdin()
-        .read_line(&mut message)
-        .expect("Could Not Read Input");
-
-    let alphabet = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
-    let mut result: Vec<char> = vec![];
-
-    message.truncate(message.len() - 2);
-    let chars = message.chars();
-    for (count, mut char) in chars.enumerate() {
-        let mut index = alphabet.iter().position(|&v| v == char).unwrap_or(25 /* Space Character in Vector */);
-
-        match index {
-            25 => { index = index - 1 }
-            26 => { index = 25 }
-            _ => {}
-        }
-
-        char = alphabet[index + 1];
-
-        result.insert(count, char)
-    }
-    let message: String = result.iter().collect();
-    println!("Ciphered Message: {:?}", message)
+    init();
+    cipher();
 }
