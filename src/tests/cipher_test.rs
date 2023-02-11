67e8f51513f463bc93bf3723b811de7877b660b9
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-02-11 21:03:48 +0000

    Tests: All Cipher Test Cases

--- src/tests/cipher_test.rs
+++ src/tests/cipher_test.rs
@@ -1,7 +1,41 @@
-use crate::cipher::cipher;
+use crate::cipher::cipher_text;
 
 #[test]
 fn cipher_lowercase_text() {
-  cipher()
+  let message = String::from("hello world");
+  let ciphered_text = cipher_text(message);
+  
+  assert_eq!(ciphered_text, "ifmmp xpsme")
+}
 
+#[test]
+fn cipher_uppercase_text() {
+  let message = String::from("HELLO WORLD");
+  let ciphered_text = cipher_text(message);
+  
+  assert_eq!(ciphered_text, "IFMMP XPSME")
+}
+
+#[test]
+fn cipher_symbols() {
+  let message = String::from("!@#$%^&*()_+{}");
+  let ciphered_text = cipher_text(message);
+  
+  assert_eq!(ciphered_text, "!@#$%^&*()_+{}")
+}
+
+#[test]
+fn cipher_space() {
+  let message = String::from("       ");
+  let ciphered_text = cipher_text(message);
+  
+  assert_eq!(ciphered_text, "") // .trim() Trims All Whitespace
+}
+
+#[test]
+fn cipher_mix() {
+  let message = String::from("ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz !@#$%^&*()_+{}");
+  let ciphered_text = cipher_text(message);
+  
+  assert_eq!(ciphered_text, "BCDEFGHIJKLMNOPQRSTUVWXYZA bcdefghijklmnopqrstuvwxyza !@#$%^&*()_+{}")
 }
