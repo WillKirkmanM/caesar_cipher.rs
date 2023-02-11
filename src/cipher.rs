8d1da09286fbd8bb393060ee664b1b3f6500f6e6
Will <WillKirkmanM@gmail.com>
2023-02-11 18:44:34 +0000

    Master

--- src/cipher.rs
+++ src/cipher.rs
@@ -16,21 +16,19 @@ pub fn cipher() {
   let chars = message.trim().chars();
   let mut uppercase_vec: Vec<usize> = vec![];
   for (count, mut char) in chars.enumerate() {
-    if char.is_uppercase() {
+    if char.is_ascii_uppercase() {
       uppercase_vec.push(count);
       char = char.to_ascii_lowercase();
     }
-      let mut index = alphabet.iter().position(|&v| v == char).unwrap_or(26 /* Space Character in Vector */);
+    let mut index = alphabet.iter().position(|&v| v == char).unwrap_or(26 /* Space Character in Vector */);
 
-      match index {
-        25 => index = 0,
-        26 => index = 26,
-        _ => index = index + 1
-      }
-
-      char = alphabet[index];
-
-      result.insert(count, char)
+    match index {
+      25 => index = 0,
+      26 => index = 26,
+      _ => index = index + 1
+    }
+    char = alphabet[index];
+    result.insert(count, char)
   }
 
   for index in uppercase_vec {
