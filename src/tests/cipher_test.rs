16469c512cb78e3200192c49c3d0f19d6f68d4dd
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-02-11 21:14:23 +0000

    Feat: Conventional Test Declaration

--- src/tests/cipher_test.rs
+++ src/tests/cipher_test.rs
@@ -1,9 +1,10 @@
-use crate::cipher::cipher_text;
+#[cfg(test)]
+use crate::cipher::cipher_message;
 
 #[test]
 fn cipher_lowercase_text() {
   let message = String::from("hello world");
-  let ciphered_text = cipher_text(message);
+  let ciphered_text = cipher_message(message);
   
   assert_eq!(ciphered_text, "ifmmp xpsme")
 }
@@ -11,7 +12,7 @@ fn cipher_lowercase_text() {
 #[test]
 fn cipher_uppercase_text() {
   let message = String::from("HELLO WORLD");
-  let ciphered_text = cipher_text(message);
+  let ciphered_text = cipher_message(message);
   
   assert_eq!(ciphered_text, "IFMMP XPSME")
 }
@@ -19,7 +20,7 @@ fn cipher_uppercase_text() {
 #[test]
 fn cipher_symbols() {
   let message = String::from("!@#$%^&*()_+{}");
-  let ciphered_text = cipher_text(message);
+  let ciphered_text = cipher_message(message);
   
   assert_eq!(ciphered_text, "!@#$%^&*()_+{}")
 }
@@ -27,7 +28,7 @@ fn cipher_symbols() {
 #[test]
 fn cipher_space() {
   let message = String::from("       ");
-  let ciphered_text = cipher_text(message);
+  let ciphered_text = cipher_message(message);
   
   assert_eq!(ciphered_text, "") // .trim() Trims All Whitespace
 }
@@ -35,7 +36,7 @@ fn cipher_space() {
 #[test]
 fn cipher_mix() {
   let message = String::from("ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz !@#$%^&*()_+{}");
-  let ciphered_text = cipher_text(message);
+  let ciphered_text = cipher_message(message);
   
   assert_eq!(ciphered_text, "BCDEFGHIJKLMNOPQRSTUVWXYZA bcdefghijklmnopqrstuvwxyza !@#$%^&*()_+{}")
 }
