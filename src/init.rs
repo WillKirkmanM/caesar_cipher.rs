b650319e414ba7e47b0c64d6e63be5370554a6c1
Will <WillKirkmanM@gmail.com>
2023-02-11 16:22:32 +0000

    Feat: Choices.

--- src/init.rs
+++ src/init.rs
@@ -1,2 +1,28 @@
+use std::io::{stdin, stdout, Write};
+
+use super::cipher::cipher;
+use super::decipher::decipher;
+
 pub fn init() {
+  println!("1: Encode Message");
+  println!("2: Decode Message");
+
+  print!("Enter your Choice: ");
+  stdout().flush().unwrap();
+
+  let mut choice = String::new();
+  stdin()
+    .read_line(&mut choice)
+    .expect("Enter a Valid Message");
+
+  match choice.as_str().trim() {
+    "1" => cipher(),
+    "2" => decipher(),
+    _ => { 
+      print!("\x1B[2J\x1B[1;1H"); // <- Clears Terminal
+      init()
+    }
+  }
+
+
 }
